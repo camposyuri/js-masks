01addb21673a74c9e6285cd7d0f34b97f042a72c
yuri_cmps <yuri@humann.com.br>
2019-12-12 10:38:04 -0300

    Masks

--- app.js
+++ app.js
@@ -3,4 +3,25 @@ $(document).ready(function() {
     mask: ["(99) 9999-9999", "(99) 99999-9999"],
     keepStatic: true
   });
+
+  $(".mask-cpf-cnpj").inputmask({
+    mask: ["999.999.999-99", "99.999.999/9999-99"],
+    keepStatic: true
+  });
+
+  $(".mask-date").inputmask ({
+    mask: ["99/99/9999"]
+  });
+
+  $(".mask-money").inputmask('decimal', {
+    'alias': 'numeric',
+    'groupSeparator': ',',
+    'autoGroup': true,
+    'digits': 2,
+    'radixPoint': ".",
+    'digitsOptional': false,
+    'allowMinus': false,
+    'prefix': 'R$ ',
+    'placeholder': ''
+  });
 });
